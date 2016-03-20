6804341d6ea578a530388217ec148d6a9419528f
Ignat Loskutov <ignat.loskutov@gmail.com>
2016-03-21 00:08:03 +0300

    Get rid of the DLL hack

--- dirtyhacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <libloaderapi.h>
-
-HMODULE evrModule = LoadLibraryA("evr.dll");
-
-/* MFCreateDXSurfaceBuffer prototype */
-typedef HRESULT(STDAPICALLTYPE *MFCDXSB)(_In_ REFIID iid, _In_ IUnknown *unkSurface, _In_ BOOL bottomUpWhenLinera, _Out_ IMFMediaBuffer **mediaBuffer);
-
-MFCDXSB pMFCreateDXSurfaceBuffer = (MFCDXSB)GetProcAddress(evrModule, "MFCreateDXSurfaceBuffer");
